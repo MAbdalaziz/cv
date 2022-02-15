addd2ff378967289bc19396d4c52705f8c50a40b
Mohammed Abdalaziz <mohammed.abdalaziz@adesso.de>
2022-02-15 08:36:28 +0100

    Updates

---
+++ _nuxt/static/1644910567/de/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644910567",layout:"default",error:a,state:{dragdrop:{enabled:false},preview:{enabled:b},i18n:{routeParams:{}}},serverRendered:b,routePath:"\u002Fde",config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},http:{browserBaseURL:c},content:{dbHash:"b25b294c"}}}}(null,true,"\u002F"));
